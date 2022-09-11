42f37ecd26e837265f5c97979bf9784e0b432f3e
Nunuyy <amalianuy01@gmail.com>
2022-09-12 02:42:43 +0700

    feat: add section products

---
+++ src/components/Card/Product/index.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react'
+import Image from 'next/image'
+import tw, { styled } from 'twin.macro'
+import Button from '@/components/Buttons'
+
+const ContentProduct = styled.div`
+  ${tw`text-center md:px-2 place-content-between`}
+`
+const Title = styled.h1`
+  ${tw`pt-8 text-3xl font-bold`}
+`
+const SubTitle = styled.p`
+  ${tw`pb-10 text-base font-normal pt-7`}
+`
+
+interface ProductProps {
+  image: string
+  title: string
+  subtitle: string
+}
+
+const Product: FC<ProductProps> = ({ image, title, subtitle }) => (
+  <>
+    <ContentProduct>
+      <Image unoptimized={true} src={image} layout="fixed" width={149} height={149} objectFit="contain" />
+      <Title>{title}</Title>
+      <SubTitle>{subtitle}</SubTitle>
+      <Button
+        text="Try For Free"
+        tw="border-[1px] border-gray-300 bg-white shadow-sm rounded-full px-7 py-2 text-primary font-medium text-base"
+        external={true}
+        url="#" //belum diisi
+      />
+    </ContentProduct>
+  </>
+)
+
+export default Product
