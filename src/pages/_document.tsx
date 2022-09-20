bfa2839840aecd9b2610bd82e28e1fed6dc4c5f6
Nunuyy <amalianuy01@gmail.com>
2022-09-20 10:33:35 +0700

    feat: add Qontak Webchat Script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,19 @@ export default class _DOCUMENT extends Document<{
       <Html>
         <Head>
           <style data-emotion-css={this.props.ids.join(' ')} dangerouslySetInnerHTML={{ __html: this.props.css }} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              const qchatInit = document.createElement('script');
+              qchatInit.src = "https://webchat.qontak.com/qchatInitialize.js";
+              const qchatWidget = document.createElement('script');
+              qchatWidget.src = "https://webchat.qontak.com/js/app.js";
+              document.head.prepend(qchatInit);
+              document.head.prepend(qchatWidget);
+              qchatInit.onload = function () {qchatInitialize({ id: 'b24e3166-64c1-41c1-893e-230e984dacb6', code: 'wexb92CgFwzD2caRWpUJEA' })};
+          `
+            }}
+          />
         </Head>
         <body>
           <Main />
